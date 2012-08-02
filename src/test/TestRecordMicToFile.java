352632c075af11339255b5f0edaa2c8d2a291117
niktrk <niktrk@gmail.com>
2012-08-02 19:42:23 +0000

    Added new papers and presentations. Did some reorganization of docs folder.

--- src/test/TestRecordMicToFile.java
+++ src/test/TestRecordMicToFile.java
@@ -11,11 +11,12 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 
 public class TestRecordMicToFile {
 
-	public static void main(String[] args) throws LineUnavailableException, IOException {
+	public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
 
 		AudioFormat format = new AudioFormat(8000, 8, 1, true, false);
 		TargetDataLine dataLine = AudioSystem.getTargetDataLine(format);
@@ -27,7 +28,7 @@ public class TestRecordMicToFile {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		
 		long start = System.currentTimeMillis();
-		while (System.currentTimeMillis() - start < 10000) {
+		while (System.currentTimeMillis() - start < 5000) {
 			int count = dataLine.read(buffer, 0, bufferSize);
 			if (count > 0) {
 				out.write(buffer, 0, count);
@@ -37,12 +38,22 @@ public class TestRecordMicToFile {
 		dataLine.close();
 		
 		
+		System.out.println(out.size());
+		
 		byte[] outArray = out.toByteArray();
+		
+		System.out.println(outArray.length);
+		
 		AudioInputStream audioInput = new AudioInputStream(new ByteArrayInputStream(outArray), format, outArray.length/format.getFrameSize());
 		
 		if (AudioSystem.isFileTypeSupported(AudioFileFormat.Type.WAVE, audioInput)) {
 			AudioSystem.write(audioInput, AudioFileFormat.Type.WAVE, new File("test.wav"));
 		}
+		
+		audioInput = AudioSystem.getAudioInputStream(new File("test.wav"));
+		
+		System.out.println(audioInput.available());
+		System.out.println(audioInput.getFrameLength());
 		
 	}
 
