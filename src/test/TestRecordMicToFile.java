12ab76d1550cc353773983953bb6922666c54a7a
niktrk <niktrk@gmail.com>
2012-08-28 19:47:32 +0000

    Test class.

--- src/test/TestRecordMicToFile.java
+++ src/test/TestRecordMicToFile.java
@@ -3,11 +3,10 @@ package test;
 import java.util.Arrays;
 import java.util.List;
 
+import main.Constants;
 import som.Input;
 import som.SOM;
 import som.TrainingSet;
-import dsp.AudioFileUtil;
-import dsp.Constants;
 import dsp.endpointdetection.EnergyEndPointDetection;
 import dsp.endpointdetection.ThresholdEndPointDetection;
 import dsp.model.Frame;
@@ -20,14 +19,13 @@ import dsp.transformation.PreEmphasisFilter;
 import dsp.transformation.SubRange;
 import dsp.transformation.Transformation;
 import dsp.transformation.WeightedMFCC;
+import dsp.util.AudioFileUtil;
 
 public class TestRecordMicToFile {
 
 	private static final int MEL_NUMBER_OF_FILTERS = 31;// 20-40
 
 	public static void main(String[] args) {
-
-		long start = System.currentTimeMillis();
 
 		// for (int j = 5; j < 10; j++) {
 		// for (int i = 1; i <= 4; i++) {
@@ -36,12 +34,12 @@ public class TestRecordMicToFile {
 		// }
 		// }
 
-		Input[] inputs = new Input[50];
+		Input[] inputs = new Input[60];
 		int index = 0;
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 1; i <= 6; i++) {
 			for (int j = 0; j < 10; j++) {
 
-				byte[] signal = AudioFileUtil.readFromFileToByteArray("audio/" + i + "-" + j + ".wav");
+				byte[] signal = AudioFileUtil.readFromFileToByteArray("audio/nikola/" + i + "-" + j + ".wav");
 				List<Frame> frames = Frame.getFramesFromByteArray(signal, Constants.FRAME_SAMPLE_LENGTH, 0.5);
 
 				Transformation emphasize = new PreEmphasisFilter();
@@ -82,14 +80,19 @@ public class TestRecordMicToFile {
 		}
 
 		TrainingSet ts = new TrainingSet(inputs);
+		testit(ts);
+	}
 
-		SOM som = new SOM(Constants.NUMBER_OF_SOM_INPUTS, 5);
-		som.train(ts, 6000);
-		som.getStats().print();
-		som.getStats().printAggregated(10, 100);
-		som.getStats().printCoverage(10, 100);
-
-		System.out.println("Time: " + (System.currentTimeMillis() - start));
+	private static void testit(TrainingSet ts) {
+		for (int i = 0; i < 3; i++) {
+			long start = System.currentTimeMillis();
+			SOM som = new SOM(Constants.NUMBER_OF_SOM_INPUTS, 6);
+			som.train(ts, 12000);
+			// som.getStats().print();
+			som.getStats().printAggregated(10, 100);
+			som.getStats().printCoverage(10, 100);
+			System.out.println("Time: " + (System.currentTimeMillis() - start));
+		}
 	}
 
 }
